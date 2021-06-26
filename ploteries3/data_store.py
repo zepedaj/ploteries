29d36aa703fe9b27eb68d9dc49a40b9d3fe45fdf
Joaquin Zepeda <jazs.pro@gmail.com>
2021-06-25 22:18:26 -0700

    DataStore __getitem__ including table joins.

--- ploteries3/data_store.py
+++ ploteries3/data_store.py
@@ -1,10 +1,15 @@
 from sqlalchemy import (func, Table, Column, Integer, String, DateTime, select,
-                        ForeignKey, LargeBinary, create_engine, MetaData)
+                        ForeignKey, LargeBinary, create_engine, MetaData, and_,
+                        UniqueConstraint)
+import numpy as np
+import itertools as it
+from pglib.validation import checked_get_single
 from pglib.sqlalchemy import ClassType, JSONEncodedType
 from contextlib import contextmanager
 from pglib.sqlalchemy import begin_connection
 from sqlalchemy.sql.elements import BinaryExpression
 from sqlalchemy.sql import column
+from typing import Union
 
 col = column
 """
@@ -51,7 +56,7 @@ class DataStore:
 
     def get_data_handlers(self, *column_constraints: BinaryExpression, connection=None):
         """
-        Gets the data handlers satisfying the specified binary constraints. E.g., 
+        Gets the data handlers satisfying the specified binary constraints. E.g.,
 
         ```
         from ploteries3.data_store import col
@@ -87,6 +92,7 @@ class DataStore:
             Column('writer_id', ForeignKey('writers.id'), nullable=False),
             Column('data_def_id', ForeignKey('data_defs.id'), nullable=False),
             Column('bytes', LargeBinary),
+            UniqueConstraint('index', 'writer_id', 'data_def_id', name='uix_index_writer_data_def'),
             extend_existing=True)
 
         # Distinguishes between writing form different DataStore instances.
@@ -115,3 +121,102 @@ class DataStore:
             extend_existing=True)
 
         self._metadata.create_all()
+
+    def __getitem__(self, idx: Union[str, tuple, dict]):
+        """
+        Load the data in a table or table join. Can load all the data or a single record. Joins are carried out on field index, enforcing the same writer. Returns a dictionary with the follocing contents:
+        ```
+        {'meta': <ndarray, shape=(num_records,), dtype=[('index', '<i4'), ('writer_id', '<i4')]>,
+         'series': { <series name> : {'created': <ndarray, shape=(num_records,), dtype='datetime64[us]'>, 'data': <data handler dependent content>}}
+        ```
+
+        :param idx: Data name or tuple of data names (to specify a join). Alternatively, pass the data name (tuple) as field 'data' in a dictionary that can further contain field 'criterion' to specify any further criterion.
+
+        * 'data': Data name string or list of names.
+        * 'index': If set to 'latest', will return a record with the highest index (there might be more than one, of which one of those with the highest worker_id is taken). Otherwise, needs to be an index value. Ignored if ``None`` (the default).
+        * 'connection': Connection object from previously-started context if any. ``None`` by default.
+        * 'criterion': List of extra criterion to apply to the data_records_table query. Empty list ``[]`` by default.
+
+
+
+        """
+
+        # Add default values to idx.
+        if isinstance(idx, str):
+            idx = {'data': [idx]}
+        elif isinstance(idx, (list, tuple)):
+            idx = {'data': idx}
+        elif not isinstance(idx, dict):
+            raise TypeError(f'Invalid input type {type(idx)}.')
+        idx = {
+            'connection': None,
+            'criterion': [],
+            'index': None,
+            **idx}
+
+        # Build aliases and load data handlers.
+        data_records_aliases = [{
+            'name': name,
+            'handler': (handler := checked_get_single(self.get_data_handlers(col('name') == name))),
+            'alias': select(self.data_records_table).where(
+                self.data_records_table.c.data_def_id == handler.decoded_data_def.id).alias(name)}
+            for name in idx['data']]
+
+        # Build query
+        _last_table = data_records_aliases[0]['alias']
+        joined_tables = _last_table
+        for _table_to_join in (_x['alias'] for _x in data_records_aliases[1:]):
+            joined_tables = joined_tables.join(
+                _table_to_join, (and_(_last_table.c.index == _table_to_join.c.index,
+                                      _last_table.c.writer_id == _table_to_join.c.writer_id)))
+
+        qry = select(
+            # Meta
+            *[getattr(_last_table.c, col).label(f'{col}')
+              for col in ['index', 'writer_id']],
+            # Series
+            *it.chain(*[
+                [getattr(_al['alias'].c, col).label(f"{_al['name']}.{col}")
+                 for col in ['created', 'bytes']]
+                for _al in data_records_aliases])).select_from(joined_tables)
+
+        #
+        if idx['index'] is not None:
+            # Get a single record.
+            if idx['index'] == 'latest':
+                # Get the record with most recent index.
+                qry = qry.order_by(col('index').desc()).first()
+            else:
+                # Get the record with the provided index.
+                qry = qry.where(col('index') == idx['index'])
+        else:
+            # Get all records in sorted order.
+            qry = qry.order_by(col('index').asc(),
+                               col('writer_id').asc())
+
+        # Execute query.
+        with self.begin_connection(connection=idx['connection']) as connection:
+            records = connection.execute(qry).fetchall()
+
+        # Format record meta data.
+        meta = np.empty(len(records),
+                        dtype=[('index', 'i'),
+                               ('writer_id', 'i')])
+        meta['index'] = [_rec.index for _rec in records]
+        meta['writer_id'] = [_rec.writer_id for _rec in records]
+
+        # Format record created time and bytes.
+        series = {}
+        for _al in data_records_aliases:
+            name = _al['name']
+            #
+            _content = {'created': np.empty(len(records), dtype='datetime64[us]')}
+            _content['created'][:] = [
+                _rec[f'{name}.created'] for _rec in records]
+            #
+            _content['data'] = _al['handler'].merge_records_data([
+                _al['handler'].decode_record_bytes(_rec[f'{name}.bytes']) for _rec in records])
+            #
+            series[name] = _content
+
+        return {'meta': meta, 'series': series}
--- tests/ploteries3/data_store.py
+++ tests/ploteries3/data_store.py
@@ -6,7 +6,7 @@ import pglib.numpy as pgnp
 import numpy as np
 import numpy.testing as npt
 from sqlalchemy.sql import column as c
-from ploteries3.ndarray_data_handlers import UniformNDArrayDataHandler
+from ploteries3.ndarray_data_handlers import UniformNDArrayDataHandler, RaggedNDArrayDataHandler
 from pglib.sqlalchemy import ClassType
 from sqlalchemy.sql.expression import bindparam
 
@@ -16,6 +16,33 @@ def get_store():
     with NamedTemporaryFile() as tmpf:
         obj = mdl.DataStore(tmpf.name)
         yield obj
+
+
+@contextmanager
+def get_store_with_data(num_uniform=[3, 5, 2], num_ragged=[11, 6, 4, 9]):
+    with get_store() as store:
+        uniform = []
+        for uniform_k, num_time_indices in enumerate(num_uniform):
+            dh = UniformNDArrayDataHandler(store, f'uniform_{uniform_k}')
+            arrs = [
+                pgnp.random_array((10, 5, 7), dtype=[('f0', 'datetime64[s]'), ('f1', 'int')])
+                for _ in range(num_time_indices)]
+            [dh.add_data(k, _arr) for k, _arr in enumerate(arrs)]
+            uniform.append({'handler': dh, 'arrays': arrs})
+
+        ragged = []
+        for ragged_k, num_time_indices in enumerate(num_ragged):
+            dh = RaggedNDArrayDataHandler(store, f'ragged_{ragged_k}')
+            arrs = [
+                pgnp.random_array(
+                    (10 + ragged_k, 5 + ragged_k, 7 + ragged_k),
+                    dtype=[('f0', 'datetime64[s]'),
+                           ('f1', 'int')])
+                for _ in range(num_time_indices)]
+            [dh.add_data(k, _arr) for k, _arr in enumerate(arrs)]
+            ragged.append({'handler': dh, 'arrays': arrs})
+
+        yield store, uniform, ragged
 
 
 class TestDataStore(TestCase):
@@ -44,3 +71,64 @@ class TestDataStore(TestCase):
                 npt.assert_array_equal(
                     dh.load_data()['data'],
                     np.array(arrs))
+
+    def test_getitem__single_series(self):
+
+        with get_store_with_data(num_uniform := [4, 3, 5, 2], num_ragged := [3, 8, 5]) \
+                as (store, uniform, ragged):
+
+            #
+            for type_name, num, orig_data in [
+                    ('uniform', num_uniform, uniform),
+                    ('ragged', num_ragged, ragged)]:
+                for array_index in range(len(num_uniform)):
+
+                    series_name = f'{type_name}_{array_index}'
+                    #
+                    stacked_arrays = orig_data[array_index]['arrays']
+                    npt.assert_array_equal(
+                        stacked_arrays,
+                        store.get_data_handlers(
+                            mdl.col('name') == series_name)[0].load_data()['data'])
+                    npt.assert_array_equal(
+                        stacked_arrays,
+                        retrieved := store[series_name]['series'][series_name]['data'])
+
+    def test_getitem__join(self):
+        with get_store_with_data(num_uniform := [4, 3, 5, 2], num_ragged := [3, 8, 5]) \
+                as (store, uniform, ragged):
+
+            #
+            for series_specs in [
+                    (('uniform', 2),),
+                    (('uniform', 0), ('ragged', 1)),
+                    (('uniform', 3), ('ragged', 0), ('uniform', 0), ('ragged', 2)),
+                    (('uniform', 2), ('ragged', 0), ('uniform', 1)),
+            ]:
+                series_names = [f'{type_name}_{k}' for type_name, k in series_specs]
+                series_lengths = [
+                    {'uniform': num_uniform, 'ragged': num_ragged}[type_name][k]
+                    for type_name, k in series_specs]
+                orig_data = [{'uniform': uniform, 'ragged': ragged}[type_name][k]
+                             for type_name, k in series_specs]
+                joined_length = min(series_lengths)
+
+                retrieved = store[series_names]
+
+                self.assertEqual(len(retrieved['series']), len(series_names))
+                self.assertEqual(len(retrieved['meta']), joined_length)
+                for _series_name, _orig_data in zip(series_names, orig_data):
+                    npt.assert_array_equal(
+                        _orig_data['arrays'][:joined_length],
+                        retrieved['series'][_series_name]['data'])
+
+                # for _series_name in series_names:
+                #     #
+                #     stacked_arrays = orig_data[array_index]['arrays']
+                #     npt.assert_array_equal(
+                #         stacked_arrays,
+                #         store.get_data_handlers(
+                #             mdl.col('name') == _series_name)[0].load_data()['data'])
+                #     npt.assert_array_equal(
+                #         stacked_arrays,
+                #         retrieved)
