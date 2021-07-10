4494ae85d0c7fd339ed7bad54ae16c97c71ecd2b
jazs-data-drive-at-804483505793 <joaqal@gmail.com>
2021-07-09 17:51:43 -0700

    Ploteries: Shadow effect + sticky at top style for menu bar.

--- ploteries/_bin_helpers/launch.py
+++ ploteries/_bin_helpers/launch.py
@@ -83,6 +83,9 @@ def create_layout(update_interval):
                 'position': 'sticky',
                 'top': 0,
                 'backgroundColor': 'white',
+                'padding': '0.5em',
+                'marginBottom': '0.5em',
+                'boxShadow': '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
                 'zIndex': 100000}),
             dcc.Tabs(id='figure-tabs', persistence=True),
             dcc.Interval(
